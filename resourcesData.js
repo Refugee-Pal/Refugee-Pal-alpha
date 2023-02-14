580b59a8f26190f6bc54bfec3485cb468f63292e
chubbiloo <123145412+chubbiloo@users.noreply.github.com>
2023-02-13 16:55:17 -0800

    deleted gitattributes

---
+++ resourcesData.js
@@ -0,0 +1,10 @@
+var subcategories = [
+    {
+        Parent: "Housing",
+        Topic: "Finding a Place to Live",
+        Description: "This section provides information and resources for refugees on finding and maintaining affordable and safe housing.",
+        Links: ["https://www.rentboard.ca/", "https://homesforgood.ca/"],
+        Descriptions: ["The Rent Board Canada provides a comprehensive listing of rental properties available in British Columbia. You can search for rental properties by location, price range, number of bedrooms, and other factors.", "Homes for Good is a non-profit organization that provides affordable housing options for people in need. They have a variety of housing options available, including rental housing, supportive housing, and community living options."],
+        Names: ["Rental Listings", "Homes for Good"]
+    }
+]
